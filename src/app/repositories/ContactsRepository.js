bb184f9c67d2a905e55c730f57e86d32f40e7fbe
joaogasoares <joaogsoares@hotmail.com>
2023-11-12 15:18:08 -0300

    Método Update refatorado

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -37,39 +37,30 @@ class ContactsRepository {
         return row;
     }
 
+    async create({ name, email, phone, category_id }) {
+
+        const [row] = await db.query(`
+            INSERT INTO  contacts( name, email, phone, category_id)
+            VALUES($1, $2, $3, $4)
+            RETURNING *
+        `, [name, email, phone, category_id]);
+        return row;
+    }
+
+    async update(id, { name, email, phone, category_id }) {
+        const [row] = await db.query(`
+            UPDATE contacts 
+            SET name = $1, email = $2, phone = $3, category_id = $4
+            WHERE id = $5
+            RETURNING *
+        `, [name, email, phone, category_id, id]);
+        return row;
+    }
+
     delete(id) {
         return new Promise((resolve) => {
             contacts = contacts.filter((contact) => contact.id !== id);
             resolve();
-        });
-    }
-
-    async create({ name, email, phone, category_id }) {
-
-        const [row] = await db.query(`
-        INSERT INTO  contacts( name, email, phone, category_id)
-        VALUES($1, $2, $3, $4)
-        RETURNING *
-        `, [name, email, phone, category_id]);
-
-        return row;
-    }
-
-    update(id, { name, email, phone, category_id }) {
-        return new Promise((resolve) => {
-            const updatedContact = {
-                id,
-                name,
-                email,
-                phone,
-                category_id: category_id || v4(), // Gera um novo se não for fornecido.
-            };
-
-            contacts = contacts.map((contact) =>(
-                contact.id === id ? updatedContact : contact
-            ));
-
-            resolve(updatedContact);
         });
     }
 }
