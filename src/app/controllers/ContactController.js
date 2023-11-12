a59c2dfc80dd48f657c5cec3f5542ba080d07f20
joaogasoares <joaogsoares@hotmail.com>
2023-11-12 14:10:33 -0300

    fix do método create e refatorei os métodos find

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -25,13 +25,13 @@ class ContactController {
         const { name, email, phone, category_id } = request.body;
 
         if (!name) {
-            return response.status(400).json({ error: 'Name is required'});
+            return response.status(400).json({ error: 'Name is required' });
         }
 
         const contactExists = await ContactsRepository.findByEmail(email);
 
         if (contactExists) {
-            return response.status(400).json({ error: 'This e-mail has already been taken'});
+            return response.status(400).json({ error: 'This e-mail is already in use' });
         }
 
         const contact = await ContactsRepository.create({
